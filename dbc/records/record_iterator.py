3d7e4a5d45329641932fdce140d72f8025ef6379
jacadzaca <vitouejj@gmail.com>
2020-06-25 19:30:14 +0200

    RecordIterator now calculates size

--- dbc/records/record_iterator.py
+++ dbc/records/record_iterator.py
@@ -5,21 +5,25 @@ from dbc.dbc_header import DBCHeader
 
 class RecordIterator():
     def __init__(self, file: BinaryIO, dbc_header: DBCHeader,
-                 record_creator, field_size: int, size: int):
+                 record_creator, field_size: int):
         self._f = file
         self._header = dbc_header
         self._record_creator = record_creator
         self._field_size = field_size
-        self._size = size
 
     def __iter__(self):
         self._f.seek(self._header.size)
         return self
 
     def __next__(self):
-        if self._f.tell() > self._size:
+        if self._f.tell() > self._calculate_size():
             raise StopIteration
         return self._record_creator(*self._read_record())
+
+    # we need to calculate size every time,
+    # becouse the number of records can change
+    def _calculate_size(self):
+        return self._header.record_count * self._header.record_size
 
     def _read_record(self):
         return [bytes_util.to_int(self._f.read(self._field_size))
@@ -35,5 +39,4 @@ class RecordIterator():
     def create(cls, file_handler: BinaryIO,
                dbc_header: DBCHeader, record_creator):
         field_size = dbc_header.record_size // dbc_header.field_count
-        size = dbc_header.record_count * dbc_header.record_size
-        return cls(file_handler, dbc_header, record_creator, field_size, size)
+        return cls(file_handler, dbc_header, record_creator, field_size)
