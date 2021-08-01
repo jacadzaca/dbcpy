ba79019d1ee20c4eea81f710d79b597ff102d038
jaca <vitouejj@gmail.com>
2021-08-01 15:56:01 +0200

    users can now add records

--- dbc/dbc_file.py
+++ dbc/dbc_file.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-
 import bytes_util
 from dbc_header import DBCHeader
 from records.record_reader import RecordReader
@@ -17,8 +16,12 @@ class DBCFile():
     def write_to_file(self, f):
         string_block = [b'\0']
         string_block_offset = 1
+        # write header, reserving the first 20 bytes
         f.write(self.header.to_bytes())
+        # count the records, so we can modify the header later
+        record_count = 0
         for record in self.records:
+            record_count += 1
             for field in record.__dict__.values():
                 if isinstance(field, int):
                     f.write(bytes_util.to_bytes(field, 4))
@@ -34,7 +37,13 @@ class DBCFile():
                         f.write(bytes_util.to_bytes(strings[i][0], 4))
                     # the last, 17th field
                     f.write(bytes_util.to_bytes(field.flag, 4))
-        f.write(b''.join(string_block))
+        string_block = b''.join(string_block)
+        f.write(string_block)
+        # write the proper header
+        f.seek(0)
+        self.header.record_count = record_count
+        self.header.string_block_size = len(string_block)
+        f.write(self.header.to_bytes())
 
     @classmethod
     def from_file(cls, f, record_type):
--- dbc/records/record_iterator.py
+++ dbc/records/record_iterator.py
@@ -1,13 +1,19 @@
+import itertools
+
 class RecordIterator():
     """
     iterator for... iterating over records stored in a DBC file
     """
-    def __init__(self, f, dbc_header, record_reader, string_block):
+    def __init__(self, f, dbc_header, record_reader, string_block, new_records=iter([])):
         self._f = f
         self._header = dbc_header
         self._record_reader = record_reader
         self._string_block = string_block
         self._size = dbc_header.record_count * dbc_header.record_size
+        self._new_records = new_records
+
+    def add_records(self, *records):
+        self._new_records = itertools.chain(self._new_records, iter(records))
 
     def __iter__(self):
         self._f.seek(self._header.size)
@@ -16,5 +22,5 @@ class RecordIterator():
     def __next__(self):
         if self._f.tell() < self._size:
             return self._record_reader.read_record(self._string_block, self._f)
-        raise StopIteration
+        return next(self._new_records)
 
