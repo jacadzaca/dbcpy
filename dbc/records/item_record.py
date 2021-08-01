55fa06609a9592877a4dddd12823ed448695fdaf
jaca <vitouejj@gmail.com>
2021-08-01 14:41:31 +0200

    ItemRecord now works with the RecordReader

--- dbc/records/item_record.py
+++ dbc/records/item_record.py
@@ -1,22 +1,17 @@
-import bytes_util as bytes_util
-from dataclasses import dataclass
-from records.__init__ import NO_STRING
+import dataclasses
+import bytes_util
 
+_fields = {
+    'entry': int,
+    'clazz': int,
+    'sub_class': int,
+    'sound': int,
+    'material_id': int,
+    'display_id': int,
+    'slot_id': int,
+    'sheat_id': int
+}
 
-@dataclass
-class ItemRecord():
-    entry: int
-    clazz: int
-    sub_class: int
-    sound: int
-    material_id: int
-    display_id: int
-    slot_id: int
-    sheat_id: int
+ItemRecord = dataclasses.make_dataclass('ItemRecord', zip(_fields.keys(), _fields.values()))
+ItemRecord.field_types = staticmethod(_fields.values())
 
-    def to_bytes(self):
-        return b''.join([bytes_util.to_bytes(value, 4)
-                for value in self.__dict__.values()])
-
-    def strings(self):
-        return NO_STRING
