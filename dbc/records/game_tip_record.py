c52cd84391e4ce6b5c924d69a7da1bd995c9412c
jaca <vitouejj@gmail.com>
2021-08-01 14:41:47 +0200

    add GameTipRecord

---
+++ dbc/records/game_tip_record.py
@@ -0,0 +1,12 @@
+import dataclasses
+import bytes_util
+from loc import Loc
+
+_fields = {
+    'entry': int,
+    'tip': Loc
+}
+
+GameTipRecord = dataclasses.make_dataclass('GameTipRecord', zip(_fields.keys(), _fields.values()))
+GameTipRecord.field_types = staticmethod(_fields.values())
+
